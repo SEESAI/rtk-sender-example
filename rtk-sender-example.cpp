fc5a0e5b416cada8db1ccdd3c61018e0909ba8c6
wsolichin-sees <wsolichin@sees.ai>
2023-11-15 12:32:31 +0000

    Fix logging type

--- rtk-sender-example.cpp
+++ rtk-sender-example.cpp
@@ -43,7 +43,7 @@ int main(int argc, char* argv[])
             }
         }
         if (serial.error() != QSerialPort::NoError) {
-            std::cout << "GPS: Failed to open Serial Device" << argv[1] << ": " << serial.errorString();
+            std::cout << "GPS: Failed to open Serial Device " << argv[1] << ": " << serial.errorString().data() << std::endl;
             return 1;
         }
     }
